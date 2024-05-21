c019b89be40c09c80db8089a8055c28ca328ff0f
MR LM SIBIYA <116903062+Smindlo04@users.noreply.github.com>
2024-05-21 20:46:34 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-namespace WindowsFormsApp1
+namespace MovieDatabase
 {
     public partial class Form1 : Form
     {
