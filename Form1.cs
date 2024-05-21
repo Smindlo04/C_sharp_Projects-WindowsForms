7ce0d8affe7f8d2c72126806c8c96be6952bf4d3
MR LM SIBIYA <116903062+Smindlo04@users.noreply.github.com>
2024-05-21 20:46:07 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;//Step 1
 
-namespace FilesReading_CSharp
+namespace FilesReading
 {
     public partial class Form1 : Form
     {
