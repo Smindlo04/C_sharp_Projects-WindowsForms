77a40eeabd39a74a4571cf84d4016c7c3d3acfc4
LM_Sibiya <116903062+Smindlo04@users.noreply.github.com>
2024-05-21 03:51:02 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;//Step 1
+
+namespace FilesReading_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {             
+                //SaveDialog
+                if(saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter outFile;
+                    outFile = File.AppendText(saveDialog.FileName + ".txt");
+
+                    outFile.WriteLine(txtName.Text);
+
+                    MessageBox.Show("Data saved to text file.");
+
+                    outFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No file selected !");
+                }
+            }
+            catch(IOException exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //SaveDialog
+                if(openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    StreamReader inFile;
+                    inFile = File.OpenText(openDialog.FileName);
+
+                    while (!inFile.EndOfStream)
+                    {
+                        listOutput.Items.Add(inFile.ReadLine());
+                    }
+                    inFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No file selected !!");
+                }
+            }
+            catch(IOException exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
